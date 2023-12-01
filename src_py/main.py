6593c8c56169f1050637a06c4cd86d250ee64f55
Michael Hall <mlh0079@uah.edu>
2023-12-01 14:42:22 -0600

    - Removed return statement to avoid json datetime parsing error.

--- src_py/main.py
+++ src_py/main.py
@@ -3,7 +3,6 @@ import os
 import boto3
 import concurrent.futures
 
-
 ed_pub_account_id = os.getenv('EDPUB_ACCOUNT_ID')
 source_bucket = os.getenv('EDPUB_BUCKET')
 destination_bucket = os.getenv('DAAC_BUCKET')
@@ -49,8 +48,8 @@ def scan_ed_pub(s3_client):
 
 
 def handle_s3_event_message(event, s3_client):
-    object_key = event.get('Records')[0].get('s3').get('object').get('key')
-    return s3_client.copy_object(
+    object_key = event.get('Records')[0].get('s3').get('object').get('key').split('/', 2)[-1]
+    s3_client.copy_object(
         Bucket=destination_bucket,
         CopySource={
             'Bucket': source_bucket,
@@ -63,11 +62,9 @@ def handle_s3_event_message(event, s3_client):
 def handler(event, context):
     s3_client = boto3.client('s3')
     if event.get('Records', None):
-        ret = handle_s3_event_message(event, s3_client)
+        handle_s3_event_message(event, s3_client)
     else:
-        ret = scan_ed_pub(s3_client)
-
-    return ret
+        scan_ed_pub(s3_client)
 
 
 if __name__ == '__main__':
